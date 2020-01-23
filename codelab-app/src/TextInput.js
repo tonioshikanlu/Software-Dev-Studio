adb3ede4a690b7dbf609edf88da2a6421b6f76ef
Oluwatoni Oshikanlu <39501338+tonioshikanlu@users.noreply.github.com>
2020-01-23 11:21:39 -0800

    Added Button

---
+++ codelab-app/src/TextInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function TextInput() {
+function getUserInput() {
+const promptResponse = prompt("Please provide input");
+console.log(promptResponse);
+}
+return (
+<button onClick={() => getUserInput()}>
+Button
+</button>
+);
+}
+export default TextInput;
